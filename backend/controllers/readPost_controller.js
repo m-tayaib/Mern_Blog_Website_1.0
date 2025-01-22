80f5f5047c2aaba80e875697f3ed7ca044275fcb
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-01-22 22:14:13 +0500

    add readPost controller

--- backend/controllers/readPost_controller.js
+++ backend/controllers/readPost_controller.js
@@ -1,6 +1,20 @@
-const ReadPost = (req, res) => {
-  const { id } = req.params;
+const PostSchema = require("../models/post_models")
+const ReadPost = async (req, res) => {
 
+  const id = req.params.id
+
+  try {
+
+    if (!id) return res.status(500).json("Post Not Found")
+    const post = await PostSchema.findById(id)
+    res.status(200).json({
+      success: true,
+      post
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+
+  }
 }
 
 
